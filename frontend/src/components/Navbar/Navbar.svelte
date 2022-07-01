374c4402fefacb46ab539a739382423059f02bae
AllanCortes <allan.cortes@alumnos.ucn.cl>
2022-06-30 21:17:09 -0400

    Login con restriccion de roles y sus respectivas vistas

--- frontend/src/components/Navbar/Navbar.svelte
+++ frontend/src/components/Navbar/Navbar.svelte
@@ -8,65 +8,100 @@
     NavItem,
     NavLink,
   } from 'sveltestrap';
+  import { onMount } from "svelte";
   import { Button } from 'sveltestrap';
   import { auth } from "../../firebase";
-  import { islogged } from "../../store/store";
-  import { userApi } from "../../Api/userApi";
+  import { islogged} from "../../store/store";
+  //import { userApi } from "../../Api/userApi";
   import { navigate } from "svelte-routing";
   import { useNavigate } from "svelte-navigator";
-  const navigates = useNavigate();
+  import { getAuth,signOut,onAuthStateChanged } from "firebase/auth";
+  import { prevent_default } from 'svelte/internal';
 
-  
-  let isloggedUser =false;
-  islogged.subscribe((data)=>{
-    isloggedUser =data;
-  })
+
 
   let isOpen = false;
+  let email ;
+
+  onMount(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        email = user.email;
+        islogged.update(() => true);
+      } else {
+        islogged.update(() => false);
+      }
+    });
+  });
 
   function handleUpdate(event) {
     isOpen = event.detail.isOpen;
   }
 
-  function handleLogout(){
-    auth.signOut().then(()=>{
-      console.log("user singned out")
-      console.log(isloggedUser)
+ function logout(){
+    signOut(auth)
+    .then(() => {
+      localStorage.removeItem("admin");
+      localStorage.removeItem("uid");
+      navigate("/home");
     })
-    localStorage.clear();
-    navigates("/login");
-  };
-
+    .catch((error) => {
+      console.log(error);
+    })
+ }
 
 </script>
 
 <Navbar style="background-color: #f7bd53" light expand="md" alt="">
+  <!-- svelte-ignore a11y-missing-attribute -->
   <img src="https://i.ibb.co/1nZpY9H/Pet-Lovers-1-removebg-preview.png" width="270" height="120" />
   <NavbarToggler on:click={() => (isOpen = !isOpen)} />
   <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
     <Nav class="ms-auto" navbar>
+      {#if !$islogged}
       <NavItem>
         <NavLink href="/">Home</NavLink>
       </NavItem>
-      {#if isloggedUser =false}
+      <NavItem>
+        <NavLink href="/catalogo">Cataloge</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/login">Login</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/register">Register</NavLink>
+      </NavItem>
+      {/if}
+      {#if $islogged && email=="admin@gmail.com"}
+      <NavItem>
+        <NavLink href="/">Home</NavLink>
+      </NavItem>
         <NavItem>
-          <NavLink href="/login">Login</NavLink>
-        </NavItem>
+          <NavLink href="/addProduct">Add Product</NavLink>
+        </NavItem> 
         <NavItem>
-          <NavLink href="/register">Register</NavLink>
+          <NavLink href="/editProduct">Edit Product</NavLink>
+        </NavItem> 
+        <NavItem>
+          <a class ="nav-link" on:click={logout} href="/login">Sign out</a>
         </NavItem>
       {/if}
+      {#if $islogged && email!="admin@gmail.com"}
       <NavItem>
-        <NavLink href="/catalogo">Catalogo</NavLink>
+        <NavLink href="/">Home</NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/addProduct">AddProduct</NavLink>
+        <NavLink href='/buy'>Cataloge</NavLink>
       </NavItem>
-      {#if isloggedUser =true}
-        <NavItem>
-          <Button size="sm" on:click={handleLogout}>Sign Out</Button>
-        </NavItem>
+      <NavItem>
+        <NavLink href="/cart">Shopping Cart</NavLink>
+      </NavItem>
+      <NavItem>
+        <a class ="nav-link" on:click={logout} href="/login">Sign out</a>
+      </NavItem>
       {/if}
     </Nav>
+      
   </Collapse>
 </Navbar>
---
+++ frontend/src/components/client/cart.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import { Table } from 'sveltestrap';
+	import {onMount} from "svelte";
+	import { Styles, Button } from 'sveltestrap';
+	let selectedHeader = "stock";
+	  let ascendingOrder = true;
+  
+	let products = [];
+	const tableHeading = ["Name", "Price", "Stock", "Type", "Description","Date Added","Action"];
+  
+	onMount(async () => {
+	  const res = await fetch("http://127.0.0.1:8000/products/");
+	  products = await res.json();
+	  
+  })
+  
+	  
+	  
+	  // SORT BY NUMBER
+	  const sortByNumber = (colHeader) => {
+		  products = products.sort((obj1, obj2) => {
+			  return ascendingOrder ? Number(obj1[colHeader]) - Number(obj2[colHeader])
+			  : Number(obj2[colHeader]) - Number(obj1[colHeader])
+		
+		  });
+		  selectedHeader = colHeader;
+	  }
+   
+	  // SORT BY STRINGs
+	  const sortByString = (colHeader) => {
+		  products = products.sort((obj1, obj2) => {
+			  if (obj1[colHeader] < obj2[colHeader]) {
+					  return -1;
+			  } else if (obj1[colHeader] > obj2[colHeader]) {
+				  return 1;
+			  }
+			  return 0; //string code values are equal		
+		  });
+		  if (!ascendingOrder) {
+			  products = products.reverse()
+		  }
+		  selectedHeader = colHeader;
+	  }
+	;
+  </script>
+  
+  
+  <Table style="background-color: #cfcfcf">
+	  
+	<thead>
+	  <tr>
+		{#each tableHeading as heading}
+		<th class:highlighted={selectedHeader === heading}
+					on:click={() => (heading === "Stock" || heading === "Price"  ) ? sortByNumber(heading) : sortByString(heading)}>
+				{heading.replace("_", " ")}
+		
+  
+			{#if heading === selectedHeader && (selectedHeader=== "Stock" || selectedHeader=== "Date Added" || selectedHeader=== "Type" || selectedHeader=== "Price" )}	
+				<span class="order-icon" on:click={() => ascendingOrder = !ascendingOrder}>
+					{@html ascendingOrder ? "&#9661;" : "&#9651;"}
+				</span>		
+			{/if}	
+		  </th>	
+		{/each}
+	  </tr>
+	</thead>
+	<tbody>
+  
+	  {#each products as product }
+		<tr>
+			
+			<td>{product.product_name}</td>
+			<td>{product.price}</td>
+			<td>{product.stock}</td>
+			<td>{product.type_product}</td>
+			<td>{product.description}</td>
+			<td>{product.dateAdded}</td>
+            <td><Button color="primary"> Edit Product</Button></td>
+		</tr>
+	  {/each}
+	</tbody>
+	
+	
+  </Table>
+ 
